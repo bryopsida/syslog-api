8823a21d695beebc287c27a5421a2e0c5f81f730
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-01-04 20:22:36 -0600

    register helmet

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import helmet from '@fastify/helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -17,6 +18,8 @@ async function bootstrap() {
     },
   );
   app.useLogger(app.get(Logger));
+  app.register(helmet);
   await app.listen(3000);
+
 }
 bootstrap();
