9bd212d91735961e50ba8c4a34e8eaa8a0d05833
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-01-04 20:18:46 -0600

    wire in pino

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+    {
+      bufferLogs: true,
+    },
+  );
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
