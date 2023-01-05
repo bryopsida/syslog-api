50d4fd72f8f31b3a1fe98584c41cdff676ee8882
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-01-04 20:32:33 -0600

    mercurious

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { LoggerModule } from 'nestjs-pino';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [HealthModule, LoggerModule.forRoot()],
+  imports: [
+    HealthModule,
+    LoggerModule.forRoot(),
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      graphiql: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
