6107b48879df034e29ddf8d5d369d68a4b72ec1b
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-01-04 20:47:53 -0600

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { HealthModule } from './health/health.module';
 import { LoggerModule } from 'nestjs-pino';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     HealthModule,
     LoggerModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost:27017/syslog'),
     GraphQLModule.forRoot<MercuriusDriverConfig>({
       driver: MercuriusDriver,
       graphiql: true,
